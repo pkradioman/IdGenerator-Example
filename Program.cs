27af3ec9bd1ca941fd3fd31f07c3ea1b87ba5051
radioman <pkradioman@hotmail.com>
2019-02-25 05:31:33 +0700

    init

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using IdGen;
+using shortid;
+
+namespace IdGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var ids1 = CreateFromIdGen();
+            var ids2 = CreateFromShortId();
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine($"Id: {ids1[i]}, {ids2[i]}");
+            }
+
+        }
+
+        static List<long> CreateFromIdGen()
+        {
+            var gen = new IdGenerator(0);
+            var ids = from i in Enumerable.Range(1, 100)
+                      select gen.CreateId();
+            return ids.ToList();
+        }
+
+        static List<string> CreateFromShortId()
+        {
+            var ids = from i in Enumerable.Range(1, 100)
+                      select ShortId.Generate(true, false, 18);
+            return ids.ToList();
+        }
+    }
+}
